444f03ae8c60f627021b1f4b8f956ab09d8e88be
upadhyaydhruv <dmupadhyay12@gmail.com>
2022-01-15 22:59:19 -0500

    implemented header for PPM capture

--- custom_quad_firmware/Core/Inc/ppm.h
+++ custom_quad_firmware/Core/Inc/ppm.h
@@ -1,3 +1,21 @@
 #ifndef PPM_H
 #define PPM_H  
 
+#include <stdbool.h>
+#include <stdint.h>
+
+// Initialize PPM timer
+void PPMInit();
+
+/*
+Get data sent from PPM, based on the channel number
+
+Would have 5 channel input:
+1.) Throttle
+2.) Yaw
+3.) Pitch
+4.) Roll
+5.) Arm/Disarm Mechanism
+
+*/
+void getChannelVal(uint8_t channelNum);
